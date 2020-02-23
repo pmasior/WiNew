ddf12fe946db4e66420be4d93d4749cc87157ce5
mr50g <61390463+mr50g@users.noreply.github.com>
2020-02-23 20:22:36 +0100

    Build files

---
+++ build-win-Desktop_Qt_5_14_1_MinGW_32_bit-Release/moc_mainwindow.cpp
@@ -0,0 +1,270 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.14.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../win/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.14.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[37];
+    char stringdata0[579];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 10), // "menu_about"
+QT_MOC_LITERAL(2, 22, 0), // ""
+QT_MOC_LITERAL(3, 23, 10), // "menu_admin"
+QT_MOC_LITERAL(4, 34, 12), // "menu_license"
+QT_MOC_LITERAL(5, 47, 8), // "open_UAC"
+QT_MOC_LITERAL(6, 56, 7), // "set_UAC"
+QT_MOC_LITERAL(7, 64, 15), // "open_UpdateFrom"
+QT_MOC_LITERAL(8, 80, 14), // "set_UpdateFrom"
+QT_MOC_LITERAL(9, 95, 13), // "open_Feedback"
+QT_MOC_LITERAL(10, 109, 12), // "set_Feedback"
+QT_MOC_LITERAL(11, 122, 9), // "open_AdId"
+QT_MOC_LITERAL(12, 132, 8), // "set_AdId"
+QT_MOC_LITERAL(13, 141, 23), // "open_SuggestionTimeline"
+QT_MOC_LITERAL(14, 165, 22), // "set_SuggestionTimeline"
+QT_MOC_LITERAL(15, 188, 13), // "open_Firewall"
+QT_MOC_LITERAL(16, 202, 12), // "set_Firewall"
+QT_MOC_LITERAL(17, 215, 13), // "open_AutoPlay"
+QT_MOC_LITERAL(18, 229, 12), // "set_AutoPlay"
+QT_MOC_LITERAL(19, 242, 18), // "open_StartExplorer"
+QT_MOC_LITERAL(20, 261, 17), // "set_StartExplorer"
+QT_MOC_LITERAL(21, 279, 15), // "open_AdminTools"
+QT_MOC_LITERAL(22, 295, 17), // "open_Troubleshoot"
+QT_MOC_LITERAL(23, 313, 21), // "open_OptionalFeatures"
+QT_MOC_LITERAL(24, 335, 19), // "open_PrivacyOptions"
+QT_MOC_LITERAL(25, 355, 19), // "open_AppPermissions"
+QT_MOC_LITERAL(26, 375, 20), // "open_WindowsSecurity"
+QT_MOC_LITERAL(27, 396, 21), // "open_InstalledUpdates"
+QT_MOC_LITERAL(28, 418, 18), // "open_UserPasswords"
+QT_MOC_LITERAL(29, 437, 19), // "open_UserPasswords2"
+QT_MOC_LITERAL(30, 457, 18), // "open_SystemRestore"
+QT_MOC_LITERAL(31, 476, 18), // "open_SearchPrivacy"
+QT_MOC_LITERAL(32, 495, 16), // "open_DefaultApps"
+QT_MOC_LITERAL(33, 512, 14), // "open_Autostart"
+QT_MOC_LITERAL(34, 527, 18), // "open_InstalledApps"
+QT_MOC_LITERAL(35, 546, 13), // "open_CleanMgr"
+QT_MOC_LITERAL(36, 560, 18) // "open_Notifications"
+
+    },
+    "MainWindow\0menu_about\0\0menu_admin\0"
+    "menu_license\0open_UAC\0set_UAC\0"
+    "open_UpdateFrom\0set_UpdateFrom\0"
+    "open_Feedback\0set_Feedback\0open_AdId\0"
+    "set_AdId\0open_SuggestionTimeline\0"
+    "set_SuggestionTimeline\0open_Firewall\0"
+    "set_Firewall\0open_AutoPlay\0set_AutoPlay\0"
+    "open_StartExplorer\0set_StartExplorer\0"
+    "open_AdminTools\0open_Troubleshoot\0"
+    "open_OptionalFeatures\0open_PrivacyOptions\0"
+    "open_AppPermissions\0open_WindowsSecurity\0"
+    "open_InstalledUpdates\0open_UserPasswords\0"
+    "open_UserPasswords2\0open_SystemRestore\0"
+    "open_SearchPrivacy\0open_DefaultApps\0"
+    "open_Autostart\0open_InstalledApps\0"
+    "open_CleanMgr\0open_Notifications"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      35,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  189,    2, 0x08 /* Private */,
+       3,    0,  190,    2, 0x08 /* Private */,
+       4,    0,  191,    2, 0x08 /* Private */,
+       5,    0,  192,    2, 0x08 /* Private */,
+       6,    0,  193,    2, 0x08 /* Private */,
+       7,    0,  194,    2, 0x08 /* Private */,
+       8,    0,  195,    2, 0x08 /* Private */,
+       9,    0,  196,    2, 0x08 /* Private */,
+      10,    0,  197,    2, 0x08 /* Private */,
+      11,    0,  198,    2, 0x08 /* Private */,
+      12,    0,  199,    2, 0x08 /* Private */,
+      13,    0,  200,    2, 0x08 /* Private */,
+      14,    0,  201,    2, 0x08 /* Private */,
+      15,    0,  202,    2, 0x08 /* Private */,
+      16,    0,  203,    2, 0x08 /* Private */,
+      17,    0,  204,    2, 0x08 /* Private */,
+      18,    0,  205,    2, 0x08 /* Private */,
+      19,    0,  206,    2, 0x08 /* Private */,
+      20,    0,  207,    2, 0x08 /* Private */,
+      21,    0,  208,    2, 0x08 /* Private */,
+      22,    0,  209,    2, 0x08 /* Private */,
+      23,    0,  210,    2, 0x08 /* Private */,
+      24,    0,  211,    2, 0x08 /* Private */,
+      25,    0,  212,    2, 0x08 /* Private */,
+      26,    0,  213,    2, 0x08 /* Private */,
+      27,    0,  214,    2, 0x08 /* Private */,
+      28,    0,  215,    2, 0x08 /* Private */,
+      29,    0,  216,    2, 0x08 /* Private */,
+      30,    0,  217,    2, 0x08 /* Private */,
+      31,    0,  218,    2, 0x08 /* Private */,
+      32,    0,  219,    2, 0x08 /* Private */,
+      33,    0,  220,    2, 0x08 /* Private */,
+      34,    0,  221,    2, 0x08 /* Private */,
+      35,    0,  222,    2, 0x08 /* Private */,
+      36,    0,  223,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->menu_about(); break;
+        case 1: _t->menu_admin(); break;
+        case 2: _t->menu_license(); break;
+        case 3: _t->open_UAC(); break;
+        case 4: _t->set_UAC(); break;
+        case 5: _t->open_UpdateFrom(); break;
+        case 6: _t->set_UpdateFrom(); break;
+        case 7: _t->open_Feedback(); break;
+        case 8: _t->set_Feedback(); break;
+        case 9: _t->open_AdId(); break;
+        case 10: _t->set_AdId(); break;
+        case 11: _t->open_SuggestionTimeline(); break;
+        case 12: _t->set_SuggestionTimeline(); break;
+        case 13: _t->open_Firewall(); break;
+        case 14: _t->set_Firewall(); break;
+        case 15: _t->open_AutoPlay(); break;
+        case 16: _t->set_AutoPlay(); break;
+        case 17: _t->open_StartExplorer(); break;
+        case 18: _t->set_StartExplorer(); break;
+        case 19: _t->open_AdminTools(); break;
+        case 20: _t->open_Troubleshoot(); break;
+        case 21: _t->open_OptionalFeatures(); break;
+        case 22: _t->open_PrivacyOptions(); break;
+        case 23: _t->open_AppPermissions(); break;
+        case 24: _t->open_WindowsSecurity(); break;
+        case 25: _t->open_InstalledUpdates(); break;
+        case 26: _t->open_UserPasswords(); break;
+        case 27: _t->open_UserPasswords2(); break;
+        case 28: _t->open_SystemRestore(); break;
+        case 29: _t->open_SearchPrivacy(); break;
+        case 30: _t->open_DefaultApps(); break;
+        case 31: _t->open_Autostart(); break;
+        case 32: _t->open_InstalledApps(); break;
+        case 33: _t->open_CleanMgr(); break;
+        case 34: _t->open_Notifications(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 35)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 35;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 35)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 35;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
